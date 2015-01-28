ecd7db98d8ad3c8f76239cc6341205cbd3b20985
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-28 22:38:23 +0100

    Refactored the repository classes into a single repository.

--- src/commonworks/data/repository.py
+++ src/commonworks/data/repository.py
@@ -11,58 +11,25 @@ __version__ = '0.0.0'
 __status__ = 'Development'
 
 
-class AgreementRepository(object):
+class Repository(object):
     """
-    Repository for Agreement instances.
-    """
-    __metaclass__ = ABCMeta
+    Interface for the repository pattern.
 
-    def find_agreement_by_id(self, agreement_id):
-        pass
+    A repository works like mix between a DAO and a collection of persistent entities. Offers CRUD methods, and also
+    allows to query the existing entities.
 
-    def find_agreements_by_submitter(self, submitter_id):
-        pass
-
-    def find_agreements_by_submitter_id(self, submitter_id, agreement_number):
-        pass
-
-    def insert_agreement(self, agreement):
-        pass
-
-
-class InterestedPartyRepository(object):
-    """
-    Repository for InterestedParty instances.
+    Querying is done with the 'get' method. It receives a predicate, and all the entities fulfilling it are returned.
     """
     __metaclass__ = ABCMeta
 
-    def find_ipa_by_id(self, ipa_id):
+    def add(self, entity):
         pass
 
-    def find_ipa_by_submitter(self, submitter_id):
+    def get(self, **predicate):
         pass
 
-    def find_ipa_by_submitter_id(self, submitter_id, ipa_id):
+    def remove(self, entity):
         pass
 
-    def insert_ipa(self, ipa):
-        pass
-
-
-class WorkRepository(object):
-    """
-    Repository for Work instances.
-    """
-    __metaclass__ = ABCMeta
-
-    def find_work_by_id(self, work_id):
-        pass
-
-    def find_works_by_submitter(self, submitter_id):
-        pass
-
-    def find_works_by_submitter_id(self, submitter_id, work_number):
-        pass
-
-    def insert_work(self, work):
+    def update(self, entity):
         pass
