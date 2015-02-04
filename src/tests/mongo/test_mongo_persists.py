fe486a49254223b189db24f49c9dce45f2b0b05b
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-04 11:51:32 +0100

    Corrected an error on the mongo tests.

--- src/tests/mongo/test_mongo_persists.py
+++ src/tests/mongo/test_mongo_persists.py
@@ -34,7 +34,7 @@ class TestAgreement(unittest.TestCase):
     """
 
     def setUp(self):
-        entity = Agreement(1, 2, 'Original', datetime.date(2015, 1, 11), datetime.date(2015, 2, 11),
+        self.entity = Agreement(1, 2, 'Original', datetime.date(2015, 1, 11), datetime.date(2015, 2, 11),
                            'D', 'D', datetime.date(2015, 6, 11), 122, 'S',
                            international_standard_code=3, retention_end_date=datetime.date(2015, 3, 11),
                            prior_royalty_status_date=datetime.date(2015, 4, 11),
