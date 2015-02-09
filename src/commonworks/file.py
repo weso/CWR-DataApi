57bbe23f6b84fa124742cf71991acf0a7fea72dd
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-09 09:24:12 +0100

    Added file level validation fields to the model.

--- src/commonworks/file.py
+++ src/commonworks/file.py
@@ -87,3 +87,192 @@ class FileTag(object):
         return self._version
 
 
+class HDR(object):
+    """
+    Represents a CWR file Transmission Header (HDR).
+
+    This is a required “cover sheet” for transmissions submitted by a participant.  It will contain the file control
+    information as well as the name of the submitter.
+    """
+
+    def __init__(self, sender_id, sender_name, sender_type, creation_date, transmission_date):
+        # Sender info
+        self._sender_id = sender_id
+        self._sender_name = sender_name
+        self._sender_type = sender_type
+
+        # Dates
+        self._creation_date = creation_date
+        self._transmission_date = transmission_date
+
+    @property
+    def sender_id(self):
+        """
+        Sender ID field.
+
+        If Sender Type is equal to PB, AA, or WR, the sender  must enter their assigned CWR CAE # in this field.
+
+        These values reside in the CWR Sender ID and Codes Table. If Sender Type is equal to SO, the sending society
+        must enter their Society Code.  These values reside in the Society Code Table.
+
+        :return: the sender ID
+        """
+        return self._sender_id
+
+    @property
+    def sender_name(self):
+        """
+        Sender Name field.
+
+        The name of the sender (publisher, society, agency).
+
+        :return: name of the sender
+        """
+        return self._sender_name
+
+    @property
+    def sender_type(self):
+        """
+        Sender Type field.
+
+        Indicates if the sender of the file is a society or a publisher.
+
+        Values are PB = Publisher, SO = Society, AA = Administrative Agency, WR = Writer
+
+        :return: the sender type
+        """
+        return self._sender_type
+
+    @property
+    def transmission_date(self):
+        """
+        Transmission Date field.
+
+        The date that this file was transmitted to all receiving entities.
+
+        :return: the transmission date
+        """
+        return self._transmission_date
+
+
+class GRH(object):
+    """
+    Represents a CWR file Group Header (GRH).
+
+    The GRH record is used to indicate the presence of a group (or batch) of transactions within the file.  A group can
+    only contain one type of transaction and this is indicated in the Transaction Type field.
+    """
+
+    def __init__(self, id, transaction_type):
+        self._id = id
+        self._transaction_type = transaction_type
+
+    def id(self):
+        """
+        Group ID field.
+
+        A unique sequential number for this group within this file.
+
+        :return: the group id
+        """
+        return self._id
+
+    def transaction_type(self):
+        """
+        Transaction Type field.
+
+        Indicates the type of transactions included in this group.  Values for this field reside in the Transaction Type
+        table.
+
+        :return: the transaction type
+        """
+        return self._transaction_type
+
+
+class GRT(object):
+    """
+    Represents a CWR file Group Trailer (GRT).
+
+    The Group Trailer Record indicates the end of a group and provides both transaction and record counts for the group.
+    """
+
+    def __init__(self, id, transaction_count, record_count):
+        self._id = id
+        self._transaction_count = transaction_count
+        self._record_count = record_count
+
+    def id(self):
+        """
+        Group ID field.
+
+        A unique sequential number for this group within this file.
+
+        It is the same group id that was present on the preceding GRH record.
+
+        :return: the group id
+        """
+        return self._id
+
+    def record_count(self):
+        """
+        Record Count field.
+
+        The number of physical records included within this group including GRH and GRT records.
+
+        :return: the record count
+        """
+        return self._record_count
+
+    def transaction_count(self):
+        """
+        Transaction Count field.
+
+        The number of transactions included within this group.
+
+        :return: the number of transactions
+        """
+        return self._transaction_count
+
+
+class TRL(object):
+    """
+    Represents a CWR file Transmission Trailer (TRL).
+
+    The Transmission Trailer record indicates the end of the transmission file.  Control totals representing the number
+    of groups, transactions, and records within the file are included on this record.
+    """
+
+    def __init__(self, group_count, transaction_count, record_count):
+        self._group_count = group_count
+        self._transaction_count = transaction_count
+        self._record_count = record_count
+
+    def group_count(self):
+        """
+        Group Count field.
+
+        The number of groups included within this file.
+
+        :return: the group count
+        """
+        return self._group_count
+
+    def record_count(self):
+        """
+        Record Count field.
+
+        The number of physical records included in this file including HDR and TRL records.
+
+        :return: the record count
+        """
+        return self._record_count
+
+    def transaction_count(self):
+        """
+        Transaction Count field.
+
+        The number of transactions included within this file.
+
+        :return: the transaction count
+        """
+        return self._transaction_count
