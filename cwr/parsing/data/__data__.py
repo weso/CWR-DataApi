8a72ee3579f792914fee9f337200b88c3c730ebd
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-18 12:24:06 +0100

    Corrected text being read as binary error.

--- cwr/parsing/data/__data__.py
+++ cwr/parsing/data/__data__.py
@@ -12,7 +12,7 @@ def __path():
 
 def record_types():
     if len(_types) == 0:
-        with open(os.path.join(__path(), os.path.basename('cwr_record_type.csv')), 'rb') as csvfile:
+        with open(os.path.join(__path(), os.path.basename('cwr_record_type.csv')), 'rt') as csvfile:
             headers_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for type_row in headers_reader:
                 for t in type_row:
