bc80cfb745c0cf3105e6ea8eb6261fb83fb96d99
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-05 11:36:35 +0100

    Begins parsing work detail groups.

--- cwr/grammar/work_detail.py
+++ cwr/grammar/work_detail.py
@@ -2,7 +2,9 @@
 
 import pyparsing as pp
 
-from cwr.grammar.record import nra, work_detail
+from cwr.grammar.record import nra as rules_nr
+
+from cwr.grammar.record import work_detail as rules_work_detail
 
 
 """
@@ -17,16 +19,16 @@ __version__ = '0.0.0'
 __status__ = 'Development'
 
 # Instrumentation
-instrumentation_information = work_detail.inst_summary + pp.Optional(pp.OneOrMore(work_detail.inst_detail))
+instrumentation_information = rules_work_detail.inst_summary + pp.Optional(pp.OneOrMore(rules_work_detail.inst_detail))
 
 # Excerpts
-information_for_excerpts = work_detail.entire_work_title + pp.Optional(nra.nra_work) + pp.Optional(
-    pp.OneOrMore(nra.now))
+information_for_excerpts = rules_work_detail.entire_title + pp.Optional(rules_nr.nra_work) + pp.Optional(
+    pp.OneOrMore(rules_nr.now))
 
 # Versions
-information_for_versions = work_detail.original_title + pp.Optional(nra.nra_work) + pp.Optional(
-    pp.OneOrMore(nra.now))
+information_for_versions = rules_work_detail.version + pp.Optional(rules_nr.nra_work) + pp.Optional(
+    pp.OneOrMore(rules_nr.now))
 
 # Components
-information_for_components = work_detail.component + pp.Optional(nra.nra_work) + pp.Optional(
-    pp.OneOrMore(nra.now))
+information_for_components = rules_work_detail.component + pp.Optional(rules_nr.nra_work) + pp.Optional(
+    pp.OneOrMore(rules_nr.now))
