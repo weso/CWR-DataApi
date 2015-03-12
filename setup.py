e568d65647490a5048da5a2e1340701ce42fb431
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-12 12:34:40 +0100

    Updated the distribution files.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import ast
 import re
 
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
 
 """
@@ -14,6 +16,13 @@ __license__ = 'MIT'
 __version__ = '0.0.0'
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
+_tests_require = ['chardet']
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 with open('cwr/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
@@ -38,9 +47,11 @@ setup(
                  'Programming Language :: Python :: 2.6', 'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3', 'Programming Language :: Python :: 3.4'],
-    long_description=open("README.rst").read(),
+    long_description=long_description,
     install_requires=[
         'pyparsing',
         'pyyaml',
     ],
+    tests_require=_tests_require,
+    extras_require={'test': _tests_require},
 )
