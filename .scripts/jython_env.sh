1151833a0c7122a337841cde611954c5278a037b
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-13 09:28:04 +0100

    Added Jython back for the tests. Added Pypy 3 for the tests.

--- .scripts/jython_env.sh
+++ .scripts/jython_env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # This script sets up the Jython environment for Travis
 
-if [[ $TOXENV == "jython"* ]]; then
+if [[ $JYTHON ]]; then
+
+  echo "Jython testing active"
 
   sudo apt-get install jython
   export PYTHON_EXE=jython; jython -c "print ''"
