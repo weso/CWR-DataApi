a887f579565e5b009ee0308571b6d876208c8970
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-13 09:45:52 +0100

    Small fixes to the configuration files.

--- setup.py
+++ setup.py
@@ -50,7 +50,9 @@ setup(
                  'Programming Language :: Python', 'Programming Language :: Python :: 2',
                  'Programming Language :: Python :: 2.6', 'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.2',
-                 'Programming Language :: Python :: 3.3', 'Programming Language :: Python :: 3.4'],
+                 'Programming Language :: Python :: 3.3', 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: Implementation :: Jython',
+                 'Programming Language :: Python :: Implementation :: PyPy'],
     long_description=long_description,
     install_requires=[
         'pyparsing',
