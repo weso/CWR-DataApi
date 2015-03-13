6c337853f29015d1033a242e112e5f1f5c685b8d
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-13 10:01:21 +0100

    More changes to config files

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setup(
                  'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3', 'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: Implementation :: Jython',
-                 'Programming Language :: Python :: Implementation :: PyPy'],
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Topic :: Software Development :: Libraries :: Python Modules'],
     long_description=long_description,
     install_requires=[
         'pyparsing',
