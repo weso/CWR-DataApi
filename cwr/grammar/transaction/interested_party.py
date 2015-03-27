446057afc45aad3b2b32987430100be92b1efb7c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-27 11:59:24 +0100

    Territory in agreement records are now completely generated by the factory.

--- cwr/grammar/transaction/interested_party.py
+++ cwr/grammar/transaction/interested_party.py
@@ -4,7 +4,12 @@ import pyparsing as pp
 
 from cwr.grammar.record import publisher, non_roman_alphabet, publisher_territory, writer, writer_territory, \
     writer_publisher, \
-    agreement_territory, interested_party_agreement
+    interested_party_agreement
+from data.accessor import CWRConfiguration
+from cwr.grammar.factory.field import DefaultFieldFactory
+from data.accessor import CWRTables
+
+from cwr.grammar.factory.record import PrefixBuilder, RecordFactory
 
 
 """
@@ -15,31 +20,47 @@ __author__ = 'Bernardo Martínez Garrido'
 __license__ = 'MIT'
 __status__ = 'Development'
 
+_config = CWRConfiguration()
+
+_data = _config.load_field_config('table')
+_data.update(_config.load_field_config('common'))
+
+_factory_field = DefaultFieldFactory(_data, CWRTables())
+
+_prefixer = PrefixBuilder(_config.record_types())
+_factory_record = RecordFactory(_config.load_record_config('common'), _prefixer, _factory_field)
+
 # Original Publisher
 original_publisher_information = publisher.publisher + \
                                  pp.Optional(non_roman_alphabet.npn) + \
                                  pp.Optional(pp.OneOrMore(publisher_territory.territory))
 
 # Administrator
-administrator_information = publisher.publisher + pp.Optional(non_roman_alphabet.npn) + \
+administrator_information = publisher.publisher + \
+                            pp.Optional(non_roman_alphabet.npn) + \
                             pp.Optional(pp.OneOrMore(publisher_territory.territory))
 
 # Subpublisher
-subpublisher_information = publisher.publisher + pp.Optional(non_roman_alphabet.npn) + \
+subpublisher_information = publisher.publisher + \
+                           pp.Optional(non_roman_alphabet.npn) + \
                            pp.Optional(pp.OneOrMore(publisher_territory.territory))
 
 # Controlled writer
-controlled_writer_information = writer.writer + pp.Optional(non_roman_alphabet.nwn) + pp.Optional(
-    pp.OneOrMore(writer_territory.territory)) + pp.OneOrMore(writer_publisher.publisher)
+controlled_writer_information = writer.writer + \
+                                pp.Optional(non_roman_alphabet.nwn) + \
+                                pp.Optional(pp.OneOrMore(writer_territory.territory)) + \
+                                pp.OneOrMore(writer_publisher.publisher)
 
 # Controlled publisher
-controlled_publisher_information = original_publisher_information + pp.Optional(
-    pp.OneOrMore(administrator_information)) + pp.Optional(pp.OneOrMore(subpublisher_information)) + pp.Optional(
-    pp.OneOrMore(publisher.publisher))
+controlled_publisher_information = original_publisher_information + \
+                                   pp.Optional(pp.OneOrMore(administrator_information)) \
+                                   + pp.Optional(pp.OneOrMore(subpublisher_information)) \
+                                   + pp.Optional(pp.OneOrMore(publisher.publisher))
 
 # IPA
 ipa_information = interested_party_agreement.interested_party_agreement + pp.Optional(non_roman_alphabet.npa)
 
 # Territory
-territory_information = pp.OneOrMore(agreement_territory.territory_in_agreement) + ipa_information * 2 + \
+territory_information = pp.OneOrMore(_factory_record.get_transaction_record('territory_in_agreement')) \
+                        + ipa_information * 2 + \
                         pp.ZeroOrMore(ipa_information)
--- tests/grammar/factory/record/test_agreement_territory.py
+++ tests/grammar/factory/record/test_agreement_territory.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from cwr.grammar.record import agreement_territory
+from data.accessor import CWRConfiguration
+from cwr.grammar.factory.field import DefaultFieldFactory
+from data.accessor import CWRTables
+from cwr.grammar.factory.record import PrefixBuilder, RecordFactory
 
 
 """
@@ -22,7 +25,17 @@ class TestParseAgreementTerritory(unittest.TestCase):
     """
 
     def setUp(self):
-        self.grammar = agreement_territory.territory_in_agreement
+        _config = CWRConfiguration()
+
+        _data = _config.load_field_config('table')
+        _data.update(_config.load_field_config('common'))
+
+        _factory_field = DefaultFieldFactory(_data, CWRTables())
+
+        _prefixer = PrefixBuilder(_config.record_types())
+        _factory_record = RecordFactory(_config.load_record_config('common'), _prefixer, _factory_field)
+
+        self.grammar = _factory_record.get_transaction_record('territory_in_agreement')
 
     def test_valid(self):
         """
