fa02994a109a39a15065acb6f6a7bd86ab16b2a6
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-12 10:03:11 +0200

    Added test for the file tag decoder.

---
+++ tests/parser/dictionary/decoder/file/test_file_tag.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from cwr.parser.decoder.dictionary import FileTagDictionaryDecoder
+from cwr.file import FileTag
+
+
+"""
+Group Header to dictionary encoding tests.
+
+The following cases are tested:
+"""
+
+__author__ = 'Bernardo Martínez Garrido'
+__license__ = 'MIT'
+__version__ = '0.0.0'
+__status__ = 'Development'
+
+
+class TestFileDictionaryEncoding(unittest.TestCase):
+    def setUp(self):
+        self._decoder = FileTagDictionaryDecoder()
+
+    def test_encoded(self):
+        data = {}
+
+        data['year'] = 2015
+        data['sequence_n'] = 123
+        data['sender'] = 'SND'
+        data['receiver'] = 'RCV'
+        data['version'] = 2.1
+
+        tag = self._decoder.decode(data)
+
+        self.assertEqual(2015, tag.year)
+        self.assertEqual(123, tag.sequence_n)
+        self.assertEqual('SND', tag.sender)
+        self.assertEqual('RCV', tag.receiver)
+        self.assertEqual(2.1, tag.version)
