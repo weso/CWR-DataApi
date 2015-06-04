6f0b08160f19f902fa90400af6405d852a589bbc
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-04 10:58:58 +0200

    Added Jython for the tests.

---
+++ .scripts/test_jython.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script runs the tests using Jython
+
+if [ "$JYTHON" == "true" ]; then
+
+   git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+   export PYENV_ROOT="$HOME/.pyenv"
+   export PATH="$PYENV_ROOT/bin:$PATH"
+   pyenv install jython-2.7.0
+   pyenv global jython-2.7.0
+
+   python --version
+
+   python install tox
+
+   tox -e jython
+
+fi
