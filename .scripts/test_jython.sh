bb92f29850fc214d474f972533ce1d744c286c5b
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-04 11:43:06 +0200

    Added messages to the script.

--- .scripts/test_jython.sh
+++ .scripts/test_jython.sh
@@ -6,13 +6,17 @@ if [ "$JYTHON" == "true" ]; then
    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
    export PYENV_ROOT="$HOME/.pyenv"
    export PATH="$PYENV_ROOT/bin:$PATH"
+
+   echo "Installing Jython"
    pyenv install jython-2.7.0
    pyenv global jython-2.7.0
 
    eval "$(pyenv init -)"
 
+   echo "Interpreter version:"
    python --version
 
+   echo "Running tests"
    pip install tox
 
    tox -e py27
