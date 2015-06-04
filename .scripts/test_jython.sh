7f2416f4398379d7ecd19fb556acdf05e3bfcb5d
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-04 11:17:49 +0200

    Test changes to the Jython script.

--- .scripts/test_jython.sh
+++ .scripts/test_jython.sh
@@ -6,10 +6,12 @@ if [ "$JYTHON" == "true" ]; then
    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
    export PYENV_ROOT="$HOME/.pyenv"
    export PATH="$PYENV_ROOT/bin:$PATH"
-   pyenv install jython-2.7.0
+   pyenv install pypy-2.5.0
    mkdir ~/jython_test
    cd ~/jython_test
-   pyenv local jython-2.7.0
+   pyenv global pypy-2.5.0
+
+   eval "$(pyenv init -)"
 
    python --version
 
