22d14421b5deae866bd08685c0d2b68360905691
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-10 09:43:20 +0200

    Reverted docs deployment script.

--- .scripts/deploy_docs.sh
+++ .scripts/deploy_docs.sh
@@ -4,11 +4,7 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$DEPLOY_DOCS" == "true" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ]]; then
 
    echo "Deploying docs"
-   if [ "$TRAVIS_BRANCH" == "master" ]; then
-      curl -X POST http://readthedocs.org/build/cwr-dataapi
-   elif [ "$TRAVIS_BRANCH" == "develop" ]; then
-      curl -X POST http://readthedocs.org/build/cwr-dataapi/?version=develop
-   fi
+   curl -X POST http://readthedocs.org/build/cwr-dataapi
 
 else
 
